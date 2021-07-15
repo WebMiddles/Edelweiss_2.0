cb9828195319e263320467bca0b1aa54bc2b9ef1
burdms <burdms@gmail.com>
2021-07-15 17:17:25 +0200

    try: fullpage

---
+++ src/scripts/main.js
@@ -0,0 +1,18 @@
+// Этот файл типа даже не нужен?
+//= libraries/fullpage.js
+
+// Только вот этот?
+// И отдельный файл нужно для горизонтального скролла еще, судя по доке?
+// https://github.com/alvarotrigo/fullPage.js#use-extensions
+
+//= libraries/fullpage.extensions.min.js
+
+// test
+if (document.body.classList.contains('index')) {
+  new fullpage('#fullpage', {
+    autoScrolling: true,
+    scrollHorizontally: true,
+  });
+
+  fullpage_api.setAllowScrolling(true, 'left, right');
+}
